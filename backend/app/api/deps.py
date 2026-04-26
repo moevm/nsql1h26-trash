3e041ea15f4ec0d65637b76219f77b3b5007214a
root <root@M14-I3W302.localdomain>
2026-04-26 21:38:25 +0300

    add : tracking orders by the courier

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -16,7 +16,6 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         )
         
         print("=== PAYLOAD FROM TOKEN ===")
-        print(payload)
         
         user_data = {
             "id": payload.get("sub") or payload.get("id"),
